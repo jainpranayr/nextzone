09d1121a1e4c1420a61b7cb8f3bbc4e247530beb
Pranay Jain <jpranay688@gmail.com>
2022-02-25 22:54:49 +0530

    add: product grid

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,51 @@
 import { Layout } from '../components'
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
+import data from '../utils/data' // dummy data
 
 export default function Home() {
   return (
-    <div>
-      <Layout>
+    <Layout>
+      <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
-      </Layout>
-    </div>
+        {/* main products grid */}
+        <Grid container spacing={3}>
+          {data.products.map(product => (
+            // single product grid
+            <Grid item md={4} key={product.name}>
+              {/* product card */}
+              <Card>
+                {/* Clickable content on card */}
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={product.image}
+                    title={product.name}></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+
+                {/* buttons and price */}
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button variant='outlined' size='small' color='primary'>
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
   )
 }
