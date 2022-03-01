19630e59e2ea0144747551d240c3b833d2dc0971
Pranay Jain <jpranay688@gmail.com>
2022-03-01 13:39:01 +0530

    add: user model and dummy user data

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+// schema
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+)
+
+// if model already present do not create new one
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+export default User
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,5 +1,5 @@
 import nc from 'next-connect'
-import { Product } from '../../models'
+import { Product, User } from '../../models'
 import { db } from '../../config'
 import { data } from '../../utils'
 
@@ -8,10 +8,15 @@ const handler = nc()
 handler.get(async (req, res) => {
   // connect to database
   await db.connect()
+
   // empty database
+  await User.deleteMany()
   await Product.deleteMany()
+
   // insert  data
+  await User.insertMany(data.users)
   await Product.insertMany(data.products)
+
   // disconnect from database
   await db.disconnect()
   res.send({ message: 'products seed to database successful' })
